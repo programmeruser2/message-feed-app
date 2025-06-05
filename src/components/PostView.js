09493149913f4a5e45eb91a08b1878b1c6e0598b
programmeruser2 <69394951+programmeruser2@users.noreply.github.com>
2025-06-05 19:07:17 -0400

    fix post spacing

--- src/components/PostView.js
+++ src/components/PostView.js
@@ -4,7 +4,7 @@ export default function PostView({ id, author, contents, showMore }) {
     return (
         <div className="border-2 w-96 flex flex-col gap-5 mt-10 p-5">
             <h3 className="text-xl font-bold">{author}</h3>
-            <p>{showMore ? contents.slice(0, 100) + (contents.length > 100 ? '...' : '') : contents}</p>
+            <p className="break-all whitespace-normal">{showMore ? contents.slice(0, 40) + (contents.length > 40 ? '...' : '') : contents}</p>
             {showMore ? ( <Link href={'/view/' + id}>Read more</Link> ) : <></>}
         </div>
     )
